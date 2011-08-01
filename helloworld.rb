35466092305c3570360086d7e63ba98b013fec4e
Dmitry <masterrr1995@gmail.com>
2011-08-01 12:28:40 +0300

    change

--- helloworld.rb
+++ helloworld.rb
@@ -1,1 +1,12 @@
-puts 'Hello World!'
+puts 'Hello World puts!' #comment
+p 'Hello world p'
+puts [33, 32, 34, 33]
+p [1, 2, 3, 4]
+puts 2+2 #=> 4
+p 2+2
+
+
+=begin
+Long comment
+Long comment
+=end
