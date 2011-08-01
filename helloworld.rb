be7bb60ac0044250ca5c0dc986798646f89edbba
Dmitry <masterrr1995@gmail.com>
2011-08-01 13:24:11 +0300

    test how print works

--- helloworld.rb
+++ helloworld.rb
@@ -1,11 +1,14 @@
 puts 'Hello World puts!' #=> Hellow World puts!
 p 'Hello world p' #=> "Hello world p"
 puts [33, 32, 34, 33] #=> 33\n32\n34\n\33 #Вывод с переводом строки 
-p [1, 2, 3, 4] #=> [1, 2, 3, 4] #Вывод без перевода строки
+p [1, 2, 3, 4] #=> [1, 2, 3, 4] #Вывод без перевода строки, для отладки
 puts 2+2 #=> 4
 p 2+2 #=> 4
+print 'testprint ' #Вывод без переноса
+print 'contin'
 
 =begin
 Long comment
 Long comment
 =end
+
