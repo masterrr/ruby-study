062511307b673d65c971b6497d8c58c53942c4b0
Dmitry <masterrr1995@gmail.com>
2011-08-01 22:06:23 +0300

    test how works variable

---
+++ variable.rb
@@ -0,0 +1,21 @@
+# encoding:utf-8
+variable1 = 'sometext' #Присваивание строки переменной
+puts variable1 #Вывод переменной variable1
+puts variable1 + variable1 #=> variable1variable1
+name = 'Дмитрий'
+puts 'Меня зовут ' + name + '.'
+
+name1 = 'Leha'
+puts name1
+name1 = 'Alex' #Переприсваивание
+puts name1
+
+### variable=variable
+var1 = 3
+var2 = var1
+puts var1
+puts var2
+puts ''
+var1 = 'три'
+puts var1
+puts var2
