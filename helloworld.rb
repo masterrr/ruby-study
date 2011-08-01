a27e35278b1620799e685c7eeb10199ab7f625e4
Dmitry <masterrr1995@gmail.com>
2011-08-01 12:32:10 +0300

    change2

--- helloworld.rb
+++ helloworld.rb
@@ -1,10 +1,9 @@
-puts 'Hello World puts!' #comment
-p 'Hello world p'
-puts [33, 32, 34, 33]
-p [1, 2, 3, 4]
+puts 'Hello World puts!' #=> Hellow World puts!
+p 'Hello world p' #=> "Hello world p"
+puts [33, 32, 34, 33] #=> 33\n32\n34\n\33 #Вывод с переводом строки 
+p [1, 2, 3, 4] #=> [1, 2, 3, 4] #Вывод без перевода строки
 puts 2+2 #=> 4
-p 2+2
-
+p 2+2 #=> 4
 
 =begin
 Long comment
