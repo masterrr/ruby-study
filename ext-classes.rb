01db619c148d88697b998325380f38fafe6d99c8
Dmitry <masterrr1995@gmail.com>
2011-08-22 14:31:02 +0300

    add comments

--- ext-classes.rb
+++ ext-classes.rb
@@ -1,7 +1,7 @@
 # encoding:utf-8
-class Integer
+class Integer #определение метода в классе integer
   def to_eng
-    if self == 1
+    if self == 1 # self - ссылка на объект, использующий этот метод
       english = 'one'
     else
       english = 'some number, not 1 :)'
